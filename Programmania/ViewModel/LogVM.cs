05d55156ccb7cc371e684676ee4d108933174004
emil.abbas.2408 <emil.abbas.2408@gmail.com>
2021-08-14 22:33:34 +0400

    Update for authorization registration

---
+++ Programmania/ViewModel/LogVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Programmania.ViewModel
+{
+    public class LogVM
+    {
+        public LogVM()
+        {
+
+        }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
