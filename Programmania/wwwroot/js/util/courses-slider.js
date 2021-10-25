e3f90fe8a8b8f516567439cf166673602e425e71
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2021-10-25 13:39:17 +0400

    Change Courses part in Main page

--- Programmania/wwwroot/js/util/courses-slider.js
+++ Programmania/wwwroot/js/util/courses-slider.js
@@ -1,22 +1,11 @@
 new Swiper('.courses', {
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
+	autoplay: {
+		delay: 5000,
+
+		stopOnLastSlide: false,
+		disableOnInteraction: false
 	},
-	slidesPerView: 1,
-	centeredSlides: true,
-	spaceBetween: 30,
-	speed: 800,
 	loop: true,
-	breakpoints: {
-		1500: {
-			slidesPerView: 4
-        },
-		1200: {
-			slidesPerView: 3
-		},
-		800: {
-			slidesPerView: 2
-		}
-	}
+	speed: 500,
+	effect: 'fade',
 });
