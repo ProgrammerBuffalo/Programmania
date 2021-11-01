f81a0900e3eb8211dd30e40e2329b89a2d5f06b7
ProgrammerBuffalo <eldareyvazov@gmail.com>
2021-11-01 15:22:25 +0400

    updates in challenge controller and in view models

--- Programmania/Controllers/ChallengeController.cs
+++ Programmania/Controllers/ChallengeController.cs
@@ -3,19 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Programmania.Attributes;
 using Programmania.Models;
+using Programmania.Services;
+using Programmania.ViewModels;
 
 namespace Programmania.Controllers
 {
+    [Route("Challenges")]
     [Authorize]
     public class ChallengeController : Controller
     {
+        private DAL.ProgrammaniaDBContext dbContext;
+        private IFileService fileService;
+
+        public ChallengeController(DAL.ProgrammaniaDBContext context, IFileService fileService)
+        {
+            this.dbContext = context;
+            this.fileService = fileService;
+        }
+
         [HttpGet]
         public IActionResult GetChallenges()
         {
             var user = HttpContext.Items["User"] as User;
-            return View();
+            List<ChallengeVM> challenges =
+                dbContext.UserChallenges.Where(uc => uc.UserId == user.Id && !uc.IsFinished).
+                                         Select(s => new ChallengeVM
+                                         {
+                                             Id = s.ChallengeId,
+                                             Course = s.Challenge.Course.Name,
+                                             Date = s.Challenge.Created,
+                                             OpponentDescription = new UserShortDescriptionVM
+                                             {
+                                                 Id = s.UserId,
+                                                 Name = s.User.Name,
+                                                 Avatar = fileService.GetDocument(dbContext.Documents
+                                                                     .FirstOrDefault(d => d.StreamId == s.User.ImageId).Path)
+                                             }
+                                         }).ToList();
+            return View(challenges);
         }
 
         [HttpPost]
@@ -24,12 +53,24 @@ namespace Programmania.Controllers
             return Ok();
         }
 
-        public IActionResult CreateChallenge(int courseId)
+        [Route("create-challenge")]
+        [HttpPost]
+        public async Task<IActionResult> CreateChallenge(int courseId)
         {
-            return Ok();
+            var challengeParam = new SqlParameter
+            {
+                ParameterName = "challengeId",
+                DbType = System.Data.DbType.Int32,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            await dbContext.Database.ExecuteSqlRawAsync("exec dbo.GenerateChallengePROC @courseId, @challengeId OUT", courseId, challengeParam);
+            return RedirectToAction("accept-challenge", (int)challengeParam.Value);
         }
 
-        public IActionResult BeginChallenge(int challengeId)
+        [Route("accept-challenge")]
+        [HttpPost]
+        public IActionResult AcceptChallenge(int challengeId)
         {
             return Ok();
         }
---
+++ Programmania/Migrations/20211101110425_ProgmanDBChallengeProgrammingObjectsMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Programmania.Migrations
+{
+    public partial class ProgmanDBChallengeProgrammingObjectsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnswersCount",
+                table: "Challenges",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            //migrationBuilder.Sql()
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswersCount",
+                table: "Challenges");
+        }
+    }
+}
