2ec30fc6069c9cc7b66bf79ecdfabd72031b5c3f
PNik1ta <75731861+PNik1ta@users.noreply.github.com>
2021-11-05 19:14:37 +0400

    Fixed main page 1. Updated slider arrows 2. Updated challenge block 3. Updated current course block

--- Programmania/wwwroot/js/util/courses-slider.js
+++ Programmania/wwwroot/js/util/courses-slider.js
@@ -1,11 +1,13 @@
 new Swiper('.courses', {
 	autoplay: {
 		delay: 5000,
-
 		stopOnLastSlide: false,
 		disableOnInteraction: false
 	},
-	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+    },
 	speed: 500,
 	effect: 'fade',
 });
